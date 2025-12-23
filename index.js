b230550f1747a05e341b32dba18dee99051e62b4
soniyuvraj2006 <soniyuvraj2006@gmail.com>
2025-12-23 13:42:54 +0530

    Initial weather app with Docker

---
+++ index.js
@@ -0,0 +1,46 @@
+// Load environment variables
+require("dotenv").config();
+
+// Import packages
+const express = require("express");
+const axios = require("axios");
+
+// Create app
+const app = express();
+const PORT = 3000;
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Weather App is running 🌦");
+});
+
+// Weather route
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+  const apiKey = process.env.WEATHER_API_KEY;
+
+  if (!city) {
+    return res.send("Please provide a city name");
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    res.json({
+      city: data.name,
+      temperature: data.main.temp,
+      weather: data.weather[0].description
+    });
+  } catch (error) {
+    res.send("City not found");
+  }
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
